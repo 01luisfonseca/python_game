8e8846335385729065dec88603c704c4d79e8428
Luis Fonseca <01luisfonseca@gmail.com>
2025-06-04 22:30:51 -0500

    Mejorar la ventana de Pygame: se agregó un título, un ícono y un fondo gris a la pantalla.

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Invasión espacial")
+icon = pygame.image.load("alien.png")
+pygame.display.set_icon(icon)
 
 in_exec = True
 while in_exec:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             in_exec = False
+    
+    # Gray Space background
+    screen.fill((100, 100, 100))
+    pygame.display.update()
+
+pygame.quit()
