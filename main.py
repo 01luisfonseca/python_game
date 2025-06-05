43516d7ff27d6f42c47a94ce7156213d08aac41e
Luis Fonseca <01luisfonseca@gmail.com>
2025-06-05 09:32:36 -0500

    Agregar funcionalidad del jugador en main.py: se implementó el ícono del jugador, su posición inicial y controles para moverlo a la izquierda y derecha. También se ajustó el frame rate del juego.

--- main.py
+++ main.py
@@ -7,14 +7,36 @@ pygame.display.set_caption("Invasión espacial")
 icon = pygame.image.load("alien.png")
 pygame.display.set_icon(icon)
 
+# Player
+player_icon = pygame.image.load("spaceship.png") # 64x64
+# Player position. He starts in the middle of the screen, in the bottom.
+player_x = 400 - 32
+player_y = 550 - 32
+player_x_change = 0
+
+def player_draw(x, y):
+    screen.blit(player_icon, (x, y))
+
 in_exec = True
+
+frame_rate = 60
+clock = pygame.time.Clock()
+
 while in_exec:
+    # Gray Space background
+    screen.fill((100, 100, 100))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             in_exec = False
-    
-    # Gray Space background
-    screen.fill((100, 100, 100))
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_x_change = -0.5
+            if event.key == pygame.K_RIGHT:
+                player_x_change = 0.5
+
+    player_x += player_x_change
+    player_draw(player_x, player_y)
     pygame.display.update()
+    clock.tick(frame_rate)
 
 pygame.quit()
