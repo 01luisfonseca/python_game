764ba4d0d5beed3d6ed263479929182b3c7021c1
Luis Fonseca <01luisfonseca@gmail.com>
2025-06-05 14:26:44 -0500

    Implementar controles de movimiento para el jugador en ambas direcciones y agregar límites en la pantalla. Se añadió la capacidad de mover el jugador hacia arriba y abajo, ajustando la velocidad de movimiento.

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ player_icon = pygame.image.load("spaceship.png") # 64x64
 player_x = 400 - 32
 player_y = 550 - 32
 player_x_change = 0
+player_x_change_speed = 4
+player_y_change = 0
+player_y_change_speed = 4
 
 def player_draw(x, y):
     screen.blit(player_icon, (x, y))
@@ -30,11 +33,28 @@ while in_exec:
             in_exec = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player_x_change = -0.5
+                player_x_change = -player_x_change_speed
             if event.key == pygame.K_RIGHT:
-                player_x_change = 0.5
-
+                player_x_change = player_x_change_speed
+            if event.key == pygame.K_UP:
+                player_y_change = -player_y_change_speed
+            if event.key == pygame.K_DOWN:
+                player_y_change = player_y_change_speed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_x_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                player_y_change = 0
+    if player_x <= 0:
+        player_x = 0
+    elif player_x >= 736:
+        player_x = 736
+    if player_y <= 0:
+        player_y = 0
+    elif player_y >= 536:
+        player_y = 536
     player_x += player_x_change
+    player_y += player_y_change
     player_draw(player_x, player_y)
     pygame.display.update()
     clock.tick(frame_rate)
