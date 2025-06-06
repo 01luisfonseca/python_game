076777efc1ab3d174c66593f6ae38e0eb4382c62
Luis Fonseca <01luisfonseca@gmail.com>
2025-06-06 14:18:27 -0500

    Add enemy and bullet mechanics: Introduced bullet firing for enemies, implemented bullet class for projectile behavior, and added score and level display in the main game loop. Included new assets for bullets and sound effects.

--- classes.py
+++ classes.py
@@ -1,17 +1,25 @@
 import random
+from pygame import Rect
+import pygame
+from math import sqrt, atan2, degrees
+import time
 
 
 class Entity:
     def __init__(self, screen, x, y, image, border_size=10):
+        self.is_alive = True
         self.screen = screen
-        self.x = x
-        self.y = y
+        self.x = int(x)
+        self.y = int(y)
         self.image = image
         self.x_change = 0
         self.y_change = 0
         self.x_change_speed = 2
         self.y_change_speed = 2
         self.border_size = border_size
+        self.rect = Rect(
+            self.x, self.y, self.image.get_width(), self.image.get_height()
+        )
 
     def check_frontier_x_left(self):
         if self.x <= self.border_size:
@@ -40,7 +48,7 @@ class Entity:
         return True
 
     def draw(self):
-        self.screen.blit(self.image, (self.x, self.y))
+        self.screen.blit(self.image, (int(self.x), int(self.y)))
 
     def move_x(self, direction=0):
         self.x_change = self.x_change_speed * direction
@@ -58,8 +66,11 @@ class Entity:
         ):
             self.y_change = 0
 
-        self.x += self.x_change
-        self.y += self.y_change
+        self.x = int(self.x + self.x_change)
+        self.y = int(self.y + self.y_change)
+        self.rect = Rect(
+            self.x, self.y, self.image.get_width(), self.image.get_height()
+        )
 
 
 class Player(Entity):
@@ -67,18 +78,80 @@ class Player(Entity):
         super().__init__(screen, x, y, image, 5)
         self.x_change_speed = 4
         self.y_change_speed = 4
+        self.score = 0
 
 
 class Enemy(Entity):
-    def __init__(self, screen, x, y, image):
+    def __init__(self, screen, x, y, image, level=1):
         super().__init__(screen, x, y, image, 2)
-        self.x_change_speed = 5
-        self.y_change_speed = 5
-        self.x_change = 1
-        self.y_change = 1
+        self.shoot_cooldown = int(60000 / level)
+        self.last_shot = 0
+        self.can_shoot = level > 10
+        self.level = level
+        self.x_change_speed = int(5 * level)
+        self.y_change_speed = int(5 * level)
+        self.x_change = 2
+        self.y_change = 2
 
     def update_position(self):
         if random.randint(0, 100) < 5:
-            self.x_change = random.randint(-2, 2)
-            self.y_change = random.randint(-2, 2)
+            self.x_change = random.randint(-3, 3)
+            self.y_change = random.randint(-3, 3)
         super().update_position()
+
+    def shoot(self, player):
+        if self.can_shoot:
+            now = int(time.time() * 1000)
+            if now - self.last_shot > self.shoot_cooldown:
+                # Set epoch time to shoot in millis
+                self.last_shot = now
+                bullet_icon = pygame.image.load("bullet_enemy.png")
+                # Rotate the bullet icon to face the player
+                angle = atan2(player.y - self.y, player.x - self.x)
+                # Convert to degrees
+                angle = degrees(angle)
+                bullet_icon = pygame.transform.rotate(bullet_icon, angle)
+                bullet = Bullet(
+                    self.screen,
+                    self.x,
+                    self.y,
+                    bullet_icon,
+                    player.x,
+                    player.y,
+                )
+                bullet.x_change_speed = 2
+                bullet.y_change_speed = 2
+                return bullet
+        return None
+
+
+class Bullet(Entity):
+    def __init__(self, screen, x, y, image, destination_x=0, destination_y=0):
+        super().__init__(screen, x, y, image, 1)
+        self.destination_x = destination_x
+        self.destination_y = destination_y
+        self.x_change_speed = 0
+        self.y_change_speed = 7
+        self.x_change = 0
+        self.y_change = 0
+        self.delta_x = destination_x - x
+        self.delta_y = destination_y - y
+
+    def update_position(self):
+        if self.destination_x != 0 and self.destination_y != 0:
+            # Move towards the destination using a vector from x,y to destination_x,destination_y
+            newX, newY = self.new_position()
+            self.x = newX
+            self.y = newY
+        else:
+            self.y -= self.y_change_speed
+        super().update_position()
+
+    def new_position(self):
+        distance = sqrt(self.delta_x**2 + self.delta_y**2)
+        if distance == 0:
+            self.is_alive = False
+            return self.destination_x, self.destination_y
+        u_x = self.delta_x / distance
+        u_y = self.delta_y / distance
+        return self.x + u_x * self.y_change_speed, self.y + u_y * self.y_change_speed
--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 import pygame
-from classes import Player, Enemy
+from classes import Player, Enemy, Bullet
 import sys
+from pygame import mixer
+import random
 
 frame_rate = 60
 
 
-def startup():
-    pygame.init()
-
-    screen = pygame.display.set_mode((800, 600))
-    background = pygame.image.load("background2.jpg")
-    pygame.display.set_caption("Invasión espacial")
-    icon = pygame.image.load("alien.png")
-    pygame.display.set_icon(icon)
-
-    # Player position. He starts in the middle of the screen, in the bottom.
-    player = Player(
-        screen,
-        screen.get_width() / 2 - 32,
-        screen.get_height() - 64,
-        pygame.image.load("spaceship.png"),
-    )  # 64x64
-
+def restart_game(screen, player):
+    player.score = 0
+    player.is_alive = True
+    player.level = 1
+    player.x = screen.get_width() / 2 - 32
+    player.y = screen.get_height() - 64
+    player.x_change = 0
+    player.y_change = 0
     enemies = [
         Enemy(screen, 100 - 16, 50, pygame.image.load("alien.png")),
         Enemy(screen, 200 - 16, 50, pygame.image.load("alien.png")),
@@ -37,19 +30,73 @@ def startup():
         Enemy(screen, 550 - 16, 100, pygame.image.load("alien.png")),
         Enemy(screen, 650 - 16, 100, pygame.image.load("alien.png")),
     ]
+    bullets = []
+    return (enemies, bullets)
+
+
+def startup():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+    background = pygame.image.load("background2.jpg")
+    pygame.display.set_caption("Invasión espacial")
+    icon = pygame.image.load("alien.png")
+    pygame.display.set_icon(icon)
+    mixer.music.load("MusicaFondo.mp3")
+    mixer.music.set_volume(0.3)
+    mixer.music.play(-1)
+
+    # Player position. He starts in the middle of the screen, in the bottom.
+    player = Player(
+        screen,
+        screen.get_width() / 2 - 32,
+        screen.get_height() - 64,
+        pygame.image.load("spaceship.png"),
+    )  # 64x64
+
+    enemies, _ = restart_game(screen, player)
     try:
         main_loop(screen, background, player, enemies)
     except Exception:
-        print("Error: ", sys.exc_info()[0])
+        print("Error: ", sys.exc_info())
 
 
 def main_loop(screen, background, player, enemies):
+    level = 1
     in_exec = True
     clock = pygame.time.Clock()
+    bullets = []
     while in_exec:
         # Gray Space background
         screen.blit(background, (0, 0))
-        in_exec = player_movement(player)
+        draw_score(screen, player)
+        draw_level(screen, level)
+        game_over = player_movement(player, bullets, enemies)
+        if game_over and player.is_alive:
+            in_exec = False
+        elif game_over and not player.is_alive:
+            level = 1
+            enemies, bullets = restart_game(screen, player)
+        add_enemies = 0
+        for enemy in enemies:
+            if not enemy.is_alive:
+                add_enemies += 1
+                player.score += 10 * enemy.level
+        if add_enemies > 0:
+            level += 1
+            enemies.extend(
+                [
+                    Enemy(
+                        screen,
+                        random.randint(0, int(screen.get_width()) - 16),
+                        random.randint(0, int(screen.get_height() / 2 - 16)),
+                        pygame.image.load("alien.png"),
+                        level,
+                    )
+                ]
+                * add_enemies
+            )
+        enemies = [enemy for enemy in enemies if enemy.is_alive]
         for enemy in enemies:
             enemy.update_position()
             enemy.draw()
@@ -58,27 +105,117 @@ def main_loop(screen, background, player, enemies):
     pygame.quit()
 
 
-def player_movement(player):
+def draw_score(screen, player):
+    font = pygame.font.Font("Fastest.ttf", 30)
+    score_text = font.render(f"Puntaje: {player.score}", True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+
+
+def draw_level(screen, level):
+    font = pygame.font.Font("Fastest.ttf", 26)
+    level_text = font.render(f"Nivel: {level}", True, (255, 255, 255))
+    screen.blit(level_text, (10, 50))
+
+
+def game_over(screen, player):
+    font = pygame.font.Font("Fastest.ttf", 20)
+    game_over_text = font.render(
+        f"Perdiste, tu puntaje fue de {player.score} puntos",
+        True,
+        (255, 255, 255),
+    )
+    screen.blit(
+        game_over_text,
+        (
+            screen.get_width() / 2 - game_over_text.get_width() / 2,
+            screen.get_height() / 2 - game_over_text.get_height() / 2,
+        ),
+    )
+
+
+def player_movement(player, bullets, enemies):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            return False
+            return True
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT and player.is_alive:
                 player.move_x(-1)
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT and player.is_alive:
                 player.move_x(1)
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_UP and player.is_alive:
                 player.move_y(-1)
-            if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN and player.is_alive:
                 player.move_y(1)
+            if event.key == pygame.K_SPACE and player.is_alive:
+                mixer.Sound("disparo.mp3").play()
+                bulletIcon = pygame.image.load("bullet.png")
+                # Rotate the bullet icon 90 degrees clockwise
+                bulletIcon = pygame.transform.rotate(bulletIcon, -90)
+                bullets.append(
+                    Bullet(
+                        player.screen,
+                        player.x + player.image.get_width() / 2 - 8,
+                        player.y,
+                        bulletIcon,
+                    )
+                )
+            if event.key == pygame.K_ESCAPE:
+                return True
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            if (
+                event.key == pygame.K_LEFT
+                or event.key == pygame.K_RIGHT
+                and player.is_alive
+            ):
                 player.move_x(0)
-            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+            if (
+                event.key == pygame.K_UP
+                or event.key == pygame.K_DOWN
+                and player.is_alive
+            ):
                 player.move_y(0)
-    player.update_position()
-    player.draw()
-    return True
+    if player.is_alive:
+        for enemy in enemies:
+            bullet = enemy.shoot(player)
+            if bullet:
+                bullets.append(bullet)
+        player.update_position()
+        player.draw()
+    else:
+        game_over(player.screen, player)
+    # Remove bullets that are out of the screen
+    bullets = [bullet for bullet in bullets if bullet.y >= 5 and bullet.is_alive]
+    for bullet in bullets:
+        bullet.update_position()
+        bullet.draw()
+    check_collisions(bullets, enemies, player)
+    return False
+
+
+def object_collision(object1, object2):
+    return pygame.Rect.colliderect(object1.rect, object2.rect)
+
+
+def check_collisions(bullets, enemies, player):
+    for bullet in bullets:
+        if bullet.destination_x != 0 and bullet.destination_y != 0:
+            if object_collision(bullet, player) and player.is_alive:
+                mixer.Sound("dead.mp3").play()
+                player.is_alive = False
+                bullet.is_alive = False
+        for enemy in enemies:
+            if (
+                bullet.destination_x == 0
+                and bullet.destination_y == 0
+                and object_collision(bullet, enemy)
+            ):
+                mixer.Sound("Golpe.mp3").play()
+                bullet.is_alive = False
+                enemy.is_alive = False
+    for enemy in enemies:
+        if object_collision(player, enemy) and player.is_alive:
+            mixer.Sound("dead.mp3").play()
+            player.is_alive = False
 
 
 # Start the game
